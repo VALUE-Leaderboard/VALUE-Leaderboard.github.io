81cc9af54bae80029c3589c21a55a986205c3557
Rohit Pillai <ropillai@microsoft.com>
2021-02-01 12:07:53 -0800

    Made code github friendly

--- WebApplication1/ClientApp/src/App.js
+++ WebApplication1/ClientApp/src/App.js
@@ -14,12 +14,12 @@ export default class App extends Component {
     return (
         <div className="App">
             <header className="Title">
-                <NavLink to={"/"} className="tablink" activeClassName="homeActive">Home</NavLink>
+                <NavLink to={"/ValueLeaderboard"} className="tablink" activeClassName="homeActive">Home</NavLink>
                 <NavLink to={"/code"} className="tablink" activeClassName="tabActive">Code</NavLink>
                 <NavLink to={"/tasks"} className="tablink" activeClassName="tabActive">Tasks</NavLink>
                 <NavLink to={"/leaderboard"} className="tablink" activeClassName="tabActive">Leaderboard</NavLink>
             </header>
-            <Route exact path='/' component={Home} />
+            <Route exact path='/ValueLeaderboard' component={Home} />
             <Route path='/tasks' component={Tasks} />
             <Route path='/code' component={FetchData} />
             <Route path='/leaderboard' component={Leaderboard} />
