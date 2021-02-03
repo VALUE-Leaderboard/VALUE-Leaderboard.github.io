1e28a72e02173bad7651f8da4fc1a00e86c207a3
Rohit Pillai <ropillai@microsoft.com>
2021-02-02 18:22:43 -0800

    Made leaderboard sortable

--- WebApplication1/ClientApp/src/components/Leaderboard.js
+++ WebApplication1/ClientApp/src/components/Leaderboard.js
@@ -1,81 +1,158 @@
 import React, { Component } from 'react';
+import { LEADERBOARD_MODEL, METRIC_MAPS } from './LeaderboardModel';
+import './Leaderboard.css'
 
-export class Leaderboard extends Component {
+export class Leaderboard extends Component{
 
-    buildDataTable = async () => {
-        var leaderboard = [];
-        //Access the leaderboard of phase 1 of the competition 1.
-        var data = await fetch("https://codalabtest.cloudapp.net/api/competition/22705/leaderboard/data")
-        console.log(data)
-        //leaderboard.push(getJson("http://codalabtest.cloudapp.net/api/competition/1/phases/1/leaderboard/data")[0]);
-        //var tr;
-        ////clear the table
-        //$("#leaderboardtable").empty();
-        //var columnSet = [];
-        //var headerTr$ = $('<tr/>');
-        ////Get the header data
-        //var headers = leaderboard[0].headers;
-        //headerTr$.append($('<th/>').html("participant"));
-        ////Populate the header in the leaderboardtable                  
-        //for (var j = 0; j < headers.length; j++) {
-        //    headerTr$.append($('<th/>').html(headers[j].label));
-        //}
-        //$("#leaderboardtable").append(headerTr$);
-        ////Populate the scores in the leaderboardtable             
-        //for (var i = 0; i < leaderboard[0].scores.length; i++) {
-        //    tr = $('<tr/>');
-        //    tr.append("<td>" + leaderboard[0].scores[i][1].username + "</td>");
-        //    values = leaderboard[0].scores[i][1].values;
-        //    for (var j = 0; j < values.length; j++) {
-        //        tr.append("<td>" + values[j].val + "</td>");
-        //    }
-        //    $('#leaderboardtable').append(tr);
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: LEADERBOARD_MODEL,
+            direction: -1,
+            lastCol: "t12_avg_metric",
+        };
+
+        this.sortBy.bind(this);
+        this.compareBy.bind(this);
+    }
+
+    componentDidMount() {
+        //this.sortBy(this.state.lastCol);
+    }
+
+    compareBy(key, direction) {
+        console.log(this);
+        return function (a, b) {
+            var tmp1 = a[key];
+            var tmp2 = b[key];
+            if (tmp1 === '-') {
+                tmp1 = 0;
+            } 
+            if (tmp2 === '-') {
+                tmp2 = 0;
+            }
+            else if (key in METRIC_MAPS) {
+                tmp1 = 0;
+                tmp2 = 0;
+                console.log(METRIC_MAPS[key])
+                for (var i = 0; i < METRIC_MAPS[key].length; i++) {
+                    if (a[METRIC_MAPS[key][i]] !== '-') {
+                        tmp1 += a[METRIC_MAPS[key][i]];
+                    }
+                    if (b[METRIC_MAPS[key][i]] !== '-') {
+                        tmp2 += b[METRIC_MAPS[key][i]];
+                    }
+                }
+                tmp1 = tmp1 / METRIC_MAPS[key].length;
+                tmp2 = tmp2 / METRIC_MAPS[key].length;
+            }
+            if (tmp1 < tmp2) return -1 * direction;
+            if (tmp1 > tmp2) return 1 * direction;
+            return 0;
+        };
+    }
+
+    sortBy(key) {
+        if (key !== this.state.lastCol) {
+            this.setState({ direction: -1, lastCol: key })
+        }
+        else {
+            this.setState({ direction: -1 * this.state.direction })
+        }
+        let arrayCopy = [...this.state.data];
+        arrayCopy.sort(this.compareBy(key, this.state.direction));
+        this.setState({ data: arrayCopy });
+    }
+
+    buildTableRow = (model, task1, task2, task3, task4, task5, task6) => {
+
+        var t1, t2, t3, t4, t5, t6, t12_avg_metric, t34_avg_metric, t56_avg_metric;
+        if (task1 === "-") {
+            t1 = 0;
+        }
+        else {
+            t1 = task1;
+        }
+
+        if (task2 === "-") {
+            t2 = 0;
+        }
+        else {
+            t2 = task2;
+        }
+
+        t12_avg_metric = (t1 + t2) / 2;
+
+        if (task3 === "-") {
+            t3 = 0;
+        }
+        else {
+            t3 = task3;
+        }
+
+        if (task4 === "-") {
+            t4 = 0;
+        }
+        else {
+            t4 = task4;
+        }
+
+        t34_avg_metric = (t3 + t4) / 2;
+
+        if (task5 === "-") {
+            t5 = 0;
+        }
+        else {
+            t5 = task5;
+        }
+
+        if (task6 === "-") {
+            t6 = 0;
+        }
+        else {
+            t6 = task6;
+        }
+
+        t56_avg_metric = (t5 + t6) / 2;
+
+        return (
+            <tr>
+                <td> {model} </td>
+                <td> {task1} </td>
+                <td> {task2}</td>
+                <td> {t12_avg_metric} </td>
+                <td> {task3} </td>
+                <td> {task4}</td>
+                <td> {t34_avg_metric} </td>
+                <td> {task5} </td>
+                <td> {task6}</td>
+                <td> {t56_avg_metric} </td>
+            </tr>
+        );
     }
 
     render() {
+        const rows = this.state.data.map((item) => this.buildTableRow(item.model, item.task1, item.task2, item.task3, item.task4, item.task5, item.task6));
         return (
-            <div> 
-                <div className="row">
-                    <div className="large-9 columns competitions">
-                        <h2>Click below to get a list of my competitions</h2><br />
-                        <button id="show" className="button">View Competitions</button>
-                    </div>
-                    <div className="row" id="all-competitions">
-                        <div className="large-9 columns competitions">
-                            <a id="competition" href="" target="_blank">
-                                <div className="competition-tile">
-                                    <article className="subContainer">
-                                        <input id="competitionID" type="hidden" value="7" />
-                                        <div className="competitionTileLftStrip"></div>
-                                        <div className="articleImageContainer">
-                                            <div className="subContainerPadding">
-                                                <img src="" id="logo" />
-                                            </div>
-                                        </div>
-                                        <div className="articleTextArea">
-                                            <div className="subContainerPadding">
-                                                <div className="articleHeadingContainer">
-                                                    <h3 id="title"></h3>
-                                                    <label>Organized by Pragnya</label>
-                                                </div>
-                                                <div className="competition-paragraph" id="competition-paragraph">
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </article>
-                                </div>
-                            </a>
-                        </div>
-                    </div>
-                    <div className="large-9 columns competitions">
-                        <button id="showLeaderBoard" className="button" onClick={this.buildDataTable}>View LeaderBoard</button>
-                        <div className="leaderboard-result-table">
-                            <table className="resultsTable dataTable" id="leaderboardtable" border="1"></table>
-                        </div>
-                    </div>
+            <div>
+                <div class="container">
+                    <h1> Leaderboard </h1>
+                    <table id="leaderboard">
+                        <tr>
+                            <th> modelName </th>
+                            <th onClick={() => this.sortBy('task1')}> Task1</th>
+                            <th onClick={() => this.sortBy('task2')}> Task2 </th>
+                            <th onClick={() => this.sortBy('t12_avg_metric')}> TaskTypeMetric1 </th>
+                            <th onClick={() => this.sortBy('task3')}> Task3</th>
+                            <th onClick={() => this.sortBy('task4')}> Task4 </th>
+                            <th onClick={() => this.sortBy('t34_avg_metric')}> TaskTypeMetric2 </th>
+                            <th onClick={() => this.sortBy('task5')}> Task5</th>
+                            <th onClick={() => this.sortBy('task6')}> Task6 </th>
+                            <th onClick={() => this.sortBy('t56_avg_metric')}> TaskTypeMetric3 </th>
+                        </tr>
+                        {rows}
+                    </table>
                 </div>
-                <table id="leaderboardtable" border="1">
-                </table>        
             </div>
         )
     }
---
+++ WebApplication1/ClientApp/src/components/LeaderboardModel.js
@@ -0,0 +1,36 @@
+export const LEADERBOARD_MODEL = [
+    {
+        model: "m1",
+        task1: 100,
+        task2: 80,
+        task3: "-",
+        task4: "-",
+        task5: 70,
+        task6: "-"
+    },
+    {
+        model: "m2",
+        task1: 50,
+        task2: 80,
+        task3: 2,
+        task4: 25,
+        task5: 70,
+        task6: 83
+    },
+    {
+        model: "m3",
+        task1: 80,
+        task2: "-",
+        task3: "-",
+        task4: "-",
+        task5: 90,
+        task6: "-"
+    },
+]
+
+export const METRIC_MAPS = {
+    "t12_avg_metric": ["task1", "task2"],
+    "t34_avg_metric": ["task3", "task4"],
+    "t56_avg_metric": ["task5", "task6"],
+}
+
