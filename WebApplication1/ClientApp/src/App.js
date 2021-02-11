9b8801c8c5cf4e49c5fc51b522def92d9b7624c8
Rohit Pillai <ropillai@microsoft.com>
2021-02-11 01:20:36 -0800

    Added people tab to App.js

--- WebApplication1/ClientApp/src/App.js
+++ WebApplication1/ClientApp/src/App.js
@@ -3,6 +3,7 @@ import { Route, NavLink, Link } from "react-router-dom";
 import { Home } from './components/Home';
 import { Leaderboard } from './components/Leaderboard';
 import { Tasks } from './components/Tasks';
+import { People } from './components/People';
 import './App.css'
 
 export default class App extends Component {
@@ -13,10 +14,12 @@ export default class App extends Component {
         <div className="App">
             <header className="Title">
                 <NavLink exact to={"/"} className="tablink" activeClassName="tabActive">Home</NavLink>
+                <NavLink to={"/people"} className="tablink" activeClassName="tabActive">People</NavLink>
                 <NavLink to={"/tasks"} className="tablink" activeClassName="tabActive">Tasks</NavLink>
                 <NavLink to={"/leaderboard"} className="tablink" activeClassName="tabActive">Leaderboard</NavLink>
             </header>
             <Route exact path='/' component={Home} />
+            <Route path='/people' component={People} />
             <Route path='/tasks' component={Tasks} />
             <Route path='/leaderboard' component={Leaderboard} />
         </div>
