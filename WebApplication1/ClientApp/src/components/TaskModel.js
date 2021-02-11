d6873cbe88d5b9e29406d6969e3bc4256937e086
Rohit Pillai <ropillai@microsoft.com>
2021-02-11 01:22:29 -0800

    Added actual tasks and links to the tasks tab

--- WebApplication1/ClientApp/src/components/TaskModel.js
+++ WebApplication1/ClientApp/src/components/TaskModel.js
@@ -1,62 +1,44 @@
 export const TASK_MODEL = [
-    /* {
+    {
         name: "TVQA",
         download: "https://github.com/linjieli222/VALUE", 
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "Accuracy"
     },
     {
         name: "TVR",
         download: "https://github.com/linjieli222/VALUE",
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "Recall"
     },
     {
         name: "TVC",
         download: "https://github.com/linjieli222/VALUE",
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "METEOR, Rouge-L"
     },
     {
         name: "VIOLIN",
         download: "https://github.com/linjieli222/VALUE",
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "Accuracy"
     },
     {
         name: "VLEP",
         download: "https://github.com/linjieli222/VALUE",
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "Accuracy"
     },
     {
         name: "How2R",
         download: "https://github.com/linjieli222/VALUE",
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "Recall"
     },
     {
         name: "HowQA",
         download: "https://github.com/linjieli222/VALUE",
-        code: "",
+        code: "https://github.com/linjieli222/HERO",
         metric: "Accuracy"
-    } */
-    {
-        name: "Task1", 
-        download: "",
-        code: "",
-        metric: "Accuracy"
-    },
-    {
-        name: "Task2",
-        download: "",
-        code: "",
-        metric: "Precision"
-    },
-    {
-        name: "Task3",
-        download: "",
-        code: "",
-        metric: "ROUGE-L"
-    }
+    } 
 ]
